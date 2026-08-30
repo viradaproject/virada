a7af08cd6e068ef6357c22e9de23707cc905ed76
viradaproject <Viradaproject@gmail.com>
2026-08-30 10:04:31 +0200

    Add service worker registration in main.jsx
    Register service worker on window load if supported.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch((err) => {
+      console.error("No se pudo registrar el service worker:", err);
+    });
+  });
+}
